76badfe2400e73264c8736de610b6cb766b5cd14
Sar Castillo <sar.castillo@gmail.com>
2012-11-17 23:59:47 +0800

    Revert "lowmemorykiller: Use asynchronous compaction"
    This reverts commit 244a56b86acf9f38d19b41ed25d907f07701f121.

--- mm/compaction.c
+++ mm/compaction.c
@@ -681,7 +681,7 @@ unsigned long try_to_compact_pages(struct zonelist *zonelist,
 
 
 /* Compact all zones within a node */
-static int compact_node(int nid, bool sync)
+static int compact_node(int nid)
 {
 	int zoneid;
 	pg_data_t *pgdat;
@@ -699,7 +699,6 @@ static int compact_node(int nid, bool sync)
 			.nr_freepages = 0,
 			.nr_migratepages = 0,
 			.order = -1,
-            .sync = sync,
 		};
 
 		zone = &pgdat->node_zones[zoneid];
@@ -720,12 +719,12 @@ static int compact_node(int nid, bool sync)
 }
 
 /* Compact all nodes in the system */
-int compact_nodes(bool sync)
+int compact_nodes(void)
 {
 	int nid;
 
 	for_each_online_node(nid)
-		compact_node(nid, sync);
+		compact_node(nid);
 
 	return COMPACT_COMPLETE;
 }
@@ -738,7 +737,7 @@ int sysctl_compaction_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *length, loff_t *ppos)
 {
 	if (write)
-		return compact_nodes(true);
+		return compact_nodes();
 
 	return 0;
 }
