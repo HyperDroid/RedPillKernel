19f6200eaf866e80fa8cac3b0ac04b9bfea5f296
Andrei F <luxneb@gmail.com>
2013-01-12 22:15:03 +0800

    mali:add gpu oc and uv support
    Squashed all mali related commits by AndreiLux which are summarised as follows:
    1. bf3bd70
    mali: platform rewrite
    Samsung integrated checks on in the Pegasus platform that prevented the GPU control interfaces to work; on every decideNextStatus call the DVFS tables would be rewritten with separate fixed values. Module parameters for these values were present in the kernel so the hypothesis is that they were used for debugging during development.
    
    -All checks are removed and the driver now looks very similar to the Orion platform.
    
    -Removal of the CPU frequency lock to 1200MHz if the GPU is at 440MHz, this is excessive as 3D load heavy applications usually do not tax the CPU that far, and is an unnecessary power consumption burden.
    
    -The thermal control unit temperature throttling causes to fix the voltage to a fixed value when throttling is in place; this is useless considering frequency is not limited, making the whole thing senseless. Thus removed.
    
    -Cleaned up the files from unnecessary ifdef's for readability considering the fixed platform values.
    
    2. a47b74e
    mali: platform rewrite, the second encounter
    - Remove bottomlock interfaces and functions from the power interfaces and platform.
    
    - Disregard Pegasus Prime ASV voltages and voltage quirks and use common definitions.
    
    - Rewrite the scaling algorithm: we still scale step-wise up in frequency, but now we're scaling in an ondemand fashion several steps down into a target frequency able to accomodate the last load usage.
    Introduction of a CPUFreq like policy behaviour with upper and lower limit on the frequency, and together with a more fine-grained 10 step frequency table this allows for more dynamic and clear-cut scaling over the previously fixed algorithms.
    
    - Lots of refactorings and removal of unused parts.
    
    3. c0e040e
    mali: reduce utilization sampling timeout
    
    4. 5ceb84e
    mali: Add control interfaces
    Brand new interfaces for new scaling logic.
    
    5. 7bb1be6
    mali: add current_freq and time_in_state to interfaces
    Also fix the flipped input when reading in table freq and voltages.
    
    6. fb53729
    mali: Use the new sysfs_helper file, and force freqs on max & min policy updates.
    
    7. 8b75e69
    mali: Move ASV application to the init stage
    Previous to [asv: Change initcall level to an earlier stage] the ASV voltages
    would be applied too late for them being read out on ramfs post-init, or to
    be changed at boot by scripts without being overridden by the ASV application
    shortly after.
    
    Since now we are guaranteed an initialized exynos_result_of_asv, do the ASV
    application immediately on init, instead of DVFS runtime, to avoid the issue.
    
    8. a23e6f6
    mali: Add control for utilization timeout
    
    Signed-off-by: sarcastillo <sar.castillo@gmail.com>

--- drivers/media/video/samsung/mali/common/mali_kernel_utilization.c
+++ drivers/media/video/samsung/mali/common/mali_kernel_utilization.c
@@ -13,7 +13,8 @@
 #include "mali_platform.h"
 
 /* Define how often to calculate and report GPU utilization, in milliseconds */
-#define MALI_GPU_UTILIZATION_TIMEOUT 1000
+#define MALI_GPU_UTILIZATION_TIMEOUT 100
+int mali_gpu_utilization_timeout = MALI_GPU_UTILIZATION_TIMEOUT;
 
 static _mali_osk_lock_t *time_data_lock;
 
@@ -97,7 +98,7 @@ static void calculate_gpu_utilization(void *arg)
 
 	_mali_osk_lock_signal(time_data_lock, _MALI_OSK_LOCKMODE_RW);
 
-	_mali_osk_timer_add(utilization_timer, _mali_osk_time_mstoticks(MALI_GPU_UTILIZATION_TIMEOUT));
+	_mali_osk_timer_add(utilization_timer, _mali_osk_time_mstoticks(mali_gpu_utilization_timeout));
 
 	mali_gpu_utilization_handler(utilization);
 }
@@ -176,7 +177,7 @@ void mali_utilization_core_start(u64 time_now)
 
 			_mali_osk_timer_del(utilization_timer);
 
-			_mali_osk_timer_add(utilization_timer, _mali_osk_time_mstoticks(MALI_GPU_UTILIZATION_TIMEOUT));
+			_mali_osk_timer_add(utilization_timer, _mali_osk_time_mstoticks(mali_gpu_utilization_timeout));
 		} else {
 			_mali_osk_lock_signal(time_data_lock, _MALI_OSK_LOCKMODE_RW);
 		}
